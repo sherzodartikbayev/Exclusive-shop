e51264221910dbb546c1de77432b3c54182a36f1
Sherzod Artikbayev <info@web-pro.uz>
2026-01-24 20:25:09 +0500

    feat: Verify OTP

--- frontend/js/auth/verify-otp.js
+++ frontend/js/auth/verify-otp.js
@@ -1,11 +1,13 @@
 const form = document.querySelector('#form')
 const otp = document.querySelector('#otp')
+const email = document.querySelector('#email')
 const verifyBtn = document.querySelector('#verify__btn')
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
 
 	const data = {
+		email: email.value,
 		otp: otp.value,
 	}
 	console.log(data)
@@ -22,17 +24,17 @@ async function verifyOtp(data) {
 		const req = await fetch('http://localhost:8080/auth/verify-otp', {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			credentials: 'include', // SHU QATORNI QO'SHING
+			credentials: 'include',
 			body: JSON.stringify(data),
 		})
 
-		const resData = await req.json() // Serverdan kelgan xabarni o'qish uchun
+		const resData = await req.json()
 
 		if (req.ok) {
 			alert('OTP verified successfully. You can now reset your password.')
 			window.location = '../../pages/auth/reset-password.html'
 		} else {
-			alert(resData.message) // "Session expired" kabi xatolarni ko'rsatadi
+			alert(resData.message)
 		}
 	} catch (error) {
 		console.log(error)
