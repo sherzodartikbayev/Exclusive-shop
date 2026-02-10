441cb2dc9e02410c822678fdb1e417e4848edd25
Sherzod Artikbayev <info@web-pro.uz>
2026-02-10 19:57:00 +0500

    feat: Admin login

---
+++ frontend/js/admin/admin-login.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('.adminLog__form')
+const email = document.querySelector('#admin__email')
+const password = document.querySelector('#admin__password')
+const btn = document.querySelector('.adminLog__btn')
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	const data = {
+		email: email.value,
+		password: password.value,
+	}
+	console.log(data)
+
+	loginAdmin(data)
+})
+
+async function loginAdmin(data) {
+	try {
+		btn.textContent = state = 'Loading...'
+
+		const req = await fetch('http://localhost:8080/admin/login', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(data),
+		})
+
+		if (req.ok) {
+			alert('You successfully log in!')
+			window.location = '../../pages/admin/admin panel.html'
+		}
+
+		if (req.status === 404) {
+			alert('Admin not found!')
+			window.location = '../../pages/admin/admin-login.html'
+		}
+
+		if (req.status === 400) {
+			alert('Password is incorrect!')
+			window.location = '../../pages/admin/admin-login.html'
+		}
+	} catch (error) {
+		console.log(error)
+	} finally {
+		form.reset()
+		btn.textContent = state = 'Submit'
+	}
+}
