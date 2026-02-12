263503eaa391fe604d4723bd4a9af3e2561b1bbd
Sherzod Artikbayev <info@web-pro.uz>
2026-02-12 19:25:42 +0500

    feat: Integration API for Admin panel, create product, logout

---
+++ frontend/js/admin/admin-create.js
@@ -0,0 +1,52 @@
+const form = document.querySelector('.form')
+const createBtn = document.querySelector('.create')
+
+const title = document.querySelector('#title')
+const currentPrice = document.querySelector('#current-price')
+const image = document.querySelector('#image')
+const oldPrice = document.querySelector('#old-price')
+const description = document.querySelector('#description')
+
+const logoutBtn = document.querySelector('.header__navlinks__logout')
+
+logoutBtn.addEventListener('click', () => {
+	const state = confirm('Are sure log out?')
+	if (state) {
+		window.location = '../../pages/admin/admin-login.html'
+	}
+})
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	const productData = {
+		title: title.value,
+		image: image.value,
+		currentPrice: currentPrice.value,
+		oldPrice: oldPrice.value,
+		description: description.value,
+	}
+
+	createProduct(productData)
+})
+
+async function createProduct(data) {
+	try {
+		createBtn.textContent = 'Loading...'
+
+		const req = await fetch('http://localhost:8080/admin/product', {
+			headers: { 'Content-Type': 'application/json' },
+			method: 'POST',
+			body: JSON.stringify(data),
+		})
+
+		if (req.status === 201) {
+			form.reset()
+			alert('Your product successfully created!')
+			createBtn.textContent = 'Create'
+			window.location = '../../pages/admin/admin panel.html'
+		}
+	} catch (error) {
+		console.log(error)
+	}
+}
---
+++ frontend/js/admin/admin-panel.js
@@ -0,0 +1,47 @@
+const cardWrapper = document.querySelector('.products')
+const productsCount = document.querySelector('.products__count')
+const logoutBtn = document.querySelector('.header__navlinks__logout')
+
+logoutBtn.addEventListener('click', () => {
+	const state = confirm('Are sure log out?')
+	if (state) {
+		window.location = '../../pages/admin/admin-login.html'
+	}
+})
+
+async function renderProducts() {
+	try {
+		const data = await fetch('http://localhost:8080/admin/all-products').then(
+			product => product.json(),
+		)
+
+		productsCount.innerHTML = `(${data.length})`
+
+		data.map(product => {
+			const divEl = document.createElement('div')
+			divEl.classList.add('card')
+			divEl.innerHTML = `
+				<div class="img-box">
+					<img src="${product.image}" alt="${product.title}">
+				</div>
+
+				<div class="card__btns">
+					<button class="card__delete__btn">Delete</button>
+					<button class="card__edit__btn">Edit</button>
+				</div>
+
+				<h3>Gucci duffle bag</h3>
+				<div class="price">
+					<span class="new">$ ${product.currentPrice}</span>
+					<span class="old">$ ${product.oldPrice}</span>
+				</div>
+			`
+
+			cardWrapper.append(divEl)
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+renderProducts()
